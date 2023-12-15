a98295973bf3ff85ac636313ee1c6b75587b0b5a
Andrew Hopkins <anhopki@microsoft.com>
2023-12-15 13:55:58 -0500

    fetch data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO.Compression;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace csharp_neural_net;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        byte[] XTrainCompressed = Fetch("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz");
+        byte[] YTrainCompressed = Fetch("http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz");
+        byte[] XTestCompressed = Fetch("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz");
+        byte[] YTestCompressed = Fetch("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz");
+
+        byte[] XTrain = Decompress(XTrainCompressed).Skip(0x10).ToArray();
+        byte[] YTrain = Decompress(YTrainCompressed).Skip(8).ToArray();
+        byte[] XTest = Decompress(XTestCompressed).Skip(0x10).ToArray();
+        byte[] YTest = Decompress(YTestCompressed).Skip(8).ToArray();
+    }
+
+    private static byte[] Fetch(string url)
+    {
+        string filePath = Path.Combine(Path.GetTempPath(), GetMd5Hash(url));
+
+        if (File.Exists(filePath))
+        {
+            return File.ReadAllBytes(filePath);
+        }
+        else
+        {
+            byte[] data;
+            using (WebClient client = new WebClient())
+            {
+                data = client.DownloadData(url);
+            }
+
+            File.WriteAllBytes(filePath, data);
+            return data;
+        }
+    }
+
+    private static string GetMd5Hash(string input)
+    {
+        using (MD5 md5 = MD5.Create())
+        {
+            byte[] data = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+            return BitConverter.ToString(data).Replace("-", "").ToLower();
+        }
+    }
+
+    private static byte[] Decompress(byte[] data)
+    {
+        using (MemoryStream compressedStream = new MemoryStream(data))
+        using (MemoryStream decompressedStream = new MemoryStream())
+        using (GZipStream gzipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+        {
+            gzipStream.CopyTo(decompressedStream);
+            return decompressedStream.ToArray();
+        }
+    }
+}
